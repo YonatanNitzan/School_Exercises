8b058ec37fa73df9dbf393c26c54e2764b95d322
yogna <yogna@YONATAN_LAPTOP>
2017-01-18 11:30:41 +0200

    Started to prove Svetlana wrong and to replace her as the teacher

---
+++ School_Exercises/src/fuckYou_YouAreWrong.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class fuckYou_YouAreWrong {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		/*
+		int max = 0, x, y;
+		x = sc.nextInt();
+		y = sc.nextInt();
+		if(x>y)
+		{
+			max = x;
+		}
+		System.out.println("Max number=" + max);
+		*/
+		
+		/*
+		char digit;
+		char x;
+		System.out.println("Enter a digit:");
+		digit = sc.next().charAt(0);
+		if(digit == '0')
+			x = '9';
+		else
+			x = (char)(digit - 1);
+		System.out.println(x);
+		*/
+		
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		if ((a<b || a < 100) && b > 0)
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+}
