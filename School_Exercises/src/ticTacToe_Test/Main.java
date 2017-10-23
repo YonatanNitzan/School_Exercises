8d491d9b06ae4782fa78c815d65f4af45bd29244
yogna <yogna@YONATAN-LAPTOP>
2017-10-23 11:14:48 +0300

    idk

--- School_Exercises/src/ticTacToe_Test/Main.java
+++ School_Exercises/src/ticTacToe_Test/Main.java
@@ -1,8 +1,15 @@
 package ticTacToe_Test;
 
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+import ticTacToe.TTT_Grid_Piece;
+
 public class Main {
 
 	private boolean running = true;
+	private final ArrayList<Gird_Piece> grid = new ArrayList<>();
 	
 	public static void main(String[] args) {
 		new Main();
@@ -12,7 +19,10 @@ public class Main {
 	{
 		while(running)
 		{
-			
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++)
+					grid.add(null);
+			}
 		}
 	}
 
